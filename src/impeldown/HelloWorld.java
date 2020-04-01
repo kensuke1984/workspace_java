264a808dcb987933337996aa8fd22c214531f0d7
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-04-01 12:55:09 +0800

    structure

---
+++ src/impeldown/HelloWorld.java
@@ -0,0 +1,12 @@
+package impeldown;
+
+
+
+/**
+ * @author Kensuke Konishi
+ * @version 0.0.1
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/merveille/HelloWorld.java
@@ -0,0 +1,10 @@
+package merveille;
+
+/**
+ * @author Kensuke Konishi
+ * @version 0.0.1
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+    }
+}
